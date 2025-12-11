f5f9953d6b230ee8a029cf50ee33ebbacc0bc72d
Grottino <alessandro.grotta@edu-its.it>
2025-12-11 10:22:05 +0100

    feat(docker): update Docker configuration to enable Keycloak authentication

---
+++ src/main/java/com/cocktail/cocktailproject/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.cocktail.cocktailproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/cocktails",
+                                "/api/cocktails/**",
+                                "/api/cocktails/search").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/cocktails/**").hasRole("admin")
+                    .requestMatchers(HttpMethod.PUT, "/api/cocktails/**").hasRole("admin")
+                    .requestMatchers(HttpMethod.DELETE, "/api/cocktails/**").hasRole("admin")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter rolesConverter = new JwtGrantedAuthoritiesConverter();
+        rolesConverter.setAuthorityPrefix("ROLE_");
+        rolesConverter.setAuthoritiesClaimName("realm_access.roles");
+
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(rolesConverter);
+        return converter;
+    }
+}
